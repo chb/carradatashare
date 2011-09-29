c14682d99800816537af875d72c006943b5fb1cb
Dave Ortiz <daveortiz@gmail.com>
2011-09-29 12:08:28 -0400

    First Commit

---
+++ vendor/plugins/chiliproject_carranet/init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_carranet do
+  name 'Chiliproject Carranet plugin'
+  author 'David Ortiz'
+  description 'This plugin contains the modifications to ChiliProjet for the Apple Data project'
+  version '0.0.1'
+  url 'http://carranetwork.org'
+  author_url 'https://github.com/davidmortiz'
+end
