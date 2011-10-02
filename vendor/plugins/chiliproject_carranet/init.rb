a9cba9b1e004b27cecb32b50b6940bd32d3ef6f3
Dave Ortiz <daveortiz@gmail.com>
2011-10-02 19:47:35 -0400

    changes, and add .swp to .gitingore

--- vendor/plugins/chiliproject_carranet/init.rb
+++ vendor/plugins/chiliproject_carranet/init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :chiliproject_carranet do
   name 'Chiliproject Carranet plugin'
   author 'David Ortiz'
-  description 'This plugin contains the modifications to ChiliProjet for the Apple Data project'
+  description 'Chiliproject Plgin'
   version '0.0.1'
   url 'http://carranetwork.org'
-  author_url 'https://github.com/davidmortiz'
+  author_url 'http://github.org/davidmortiz'
 end
