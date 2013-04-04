85d95b2aa28eec74d55844db06bc3030b36dfc95
Dave Ortiz <daveortiz@gmail.com>
2013-04-04 16:11:16 -0400

    undoing this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,11 +24,6 @@ class ApplicationController < ActionController::Base
 
   layout 'base'
   exempt_from_layout 'builder', 'rsb'
-  
-  def ssl_required?
-    Rails.env.production?
-  end
-	
 
   protect_from_forgery
   def handle_unverified_request
