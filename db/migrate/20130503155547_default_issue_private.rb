b3dc00014d0ab396c57502589b55447479d21e02
Dave Ortiz <daveortiz@gmail.com>
2013-05-03 12:04:45 -0400

    default value for 'private' column in issues table is now true

---
+++ db/migrate/20130503155547_default_issue_private.rb
@@ -0,0 +1,9 @@
+class DefaultIssuePrivate < ActiveRecord::Migration
+  def self.up
+    change_column :issues, :private, :boolean, :default => true	  
+  end
+
+  def self.down
+    change_column :issues, :private, :boolean, :default => false	  
+  end
+end
